394114805a7ab0244bbbafa219da05d6c8308ab4
Dmitriika <104303656+Dmitriika@users.noreply.github.com>
2026-09-15 16:33:44 +0300

    Update main.py

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,8 +1,14 @@
 import typer
 app=typer.Typer()
 @app.command()
-def main(name:int):
-    print(name+name)
-
+def main(number1:int,operation:str,number2:int):
+    if operation=='+':
+        print(float(number1) + float(number2))
+    elif operation=='-':
+        print(float(number1) - float(number2))
+    elif operation in ('*','x'):
+        print(float(number1) * float(number2))
+    elif operation=='/':
+        print(float(number1) / float(number2))
 if __name__ == "__main__":
     app()
